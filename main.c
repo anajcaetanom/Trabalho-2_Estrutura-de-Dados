2d2f99e38e8065a478cd8a8da585a57a5be117a5
Ana J C Martins <anajcaetanom@gmail.com>
2023-11-21 22:06:42 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -53,5 +53,64 @@ int main() {
 
     free(arquivo);
 
+    Occupation *aparelhos = create_array(5); // Criação de uma array com 5 aparelhos disponíveis.
+    Fila *fila_para_laudo = create_fila(); // Criação da fila para laudo (vazia).
+
+    uT++; // Incremento na unidade de tempo.
+
+    printf("%d", uT);
    
+     for (int i = 0; i < 5; i++) {
+
+        /* REALIZAÇÃO DO EXAME DE RAIO-X */
+        if ((get_occupation(aparelhos, i)) == 0) {
+            srand(time(NULL));
+            int id;
+            int i = 0; 
+            int tempo_de_exame = rand() % 6 + 5;
+            change_occupation(aparelhos, i, 1); // Muda ocupação para "1" (ocupado).
+            
+            id = desenfileirar_id(fila_para_exame);
+            printf("Paciente com o id %d retirado da fila para exame.", id);
+        
+            do {
+                
+            } while (i <= tempo_de_exame);
+            puts("Exame finalizado.");
+
+            int tempo = uT + tempo_de_exame;
+
+            Registro *registro = create_registro(id, tempo, condition());
+            enfileirar_registro(fila_para_laudo, registro);
+
+            uT = tempo;
+        
+            change_occupation(aparelhos, i, 0); // Muda ocupação para "0" (livre).
+
+        uT++; // Incremento na unidade de tempo.
+        }
+    }
+    /* REALIZAÇÃO DE LAUDOS */
+
+    Occupation *radiologista = create_array(3);
+
+    for (int i = 0; i < 3; i++) {
+        if ((get_occupation(radiologista, i)) == 0) {
+            desenfileirar_registro;
+
+            // Preparação do laudo.
+            int tempo_de_exame = rand() % 20 + 10;
+            change_occupation(radiologista, i, 1); // Muda ocupação para "1" (ocupado).
+
+            do {
+
+            } while (i <= tempo_de_exame);
+            puts("Laudo finalizado.");
+
+            uT = uT + tempo_de_exame;
+
+
+        }
+   
+    }
 }
